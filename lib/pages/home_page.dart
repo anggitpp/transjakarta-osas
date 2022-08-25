a99eb41aaa6b92d9790cf8d6a1046e1f93b82429
anggitpp <anggitpp23@gmail.com>
2022-08-25 16:33:05 +0700

    add device id, and app version to app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +15,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   InAppWebViewController? webView;
+
+  String? appVersion;
+  String? deviceId;
+
+  bool isLoaded = false;
 
   void getPermission() async {
     Map<Permission, PermissionStatus> statuses = await [
@@ -30,57 +39,72 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void getApplocationDetail() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    String? id;
+
+    var deviceInfo = DeviceInfoPlugin();
+    if (Platform.isIOS) {
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      id = iosDeviceInfo.identifierForVendor; // unique ID on iOS
+    } else if (Platform.isAndroid) {
+      var androidDeviceInfo = await deviceInfo.androidInfo;
+      id = androidDeviceInfo.id; // unique ID on Android
+    }
+
+    setState(() {
+      appVersion = '${packageInfo.version}.${packageInfo.buildNumber}';
+      isLoaded = true;
+      deviceId = id;
+    });
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     getPermission();
+    getApplocationDetail();
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        // if (await webView!.canGoBack()) {
-        //   // get the webview history
-        //   WebHistory? webHistory = await webView!.getCopyBackForwardList();
-        //   // if webHistory.currentIndex corresponds to 1 or 0
-        //   if (webHistory!.currentIndex! <= 1) {
-        //     // then it means that we are on the first page
-        //     // so we can exit
-        //     return true;
-        //   }
-        //   webView!.goBack();
-        //   return false;
-        // }
         return false;
       },
       child: Scaffold(
         body: SafeArea(
-          child: InAppWebView(
-            initialUrlRequest: URLRequest(
-              url: Uri.parse("https://osas.saptasarana.net/"),
-            ),
-            initialOptions: InAppWebViewGroupOptions(
-              crossPlatform: InAppWebViewOptions(
-                supportZoom: false,
-              ),
-            ),
-            onWebViewCreated: (InAppWebViewController controller) {
-              webView = controller;
-            },
-            androidOnGeolocationPermissionsShowPrompt:
-                (InAppWebViewController controller, String origin) async {
-              return GeolocationPermissionShowPromptResponse(
-                  origin: origin, allow: true, retain: true);
-            },
-            androidOnPermissionRequest: (controller, origin, resources) async {
-              return PermissionRequestResponse(
-                  resources: resources,
-                  action: PermissionRequestResponseAction.GRANT);
-            },
-          ),
+          child: isLoaded
+              ? InAppWebView(
+                  initialUrlRequest: URLRequest(
+                    url: Uri.parse(
+                        "https://osas.saptasarana.net?version=$appVersion&device_id=$deviceId"),
+                  ),
+                  initialOptions: InAppWebViewGroupOptions(
+                    crossPlatform: InAppWebViewOptions(
+                      supportZoom: false,
+                    ),
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    webView = controller;
+                  },
+                  androidOnGeolocationPermissionsShowPrompt:
+                      (InAppWebViewController controller, String origin) async {
+                    return GeolocationPermissionShowPromptResponse(
+                        origin: origin, allow: true, retain: true);
+                  },
+                  androidOnPermissionRequest:
+                      (controller, origin, resources) async {
+                    return PermissionRequestResponse(
+                        resources: resources,
+                        action: PermissionRequestResponseAction.GRANT);
+                  },
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                ),
         ),
       ),
     );
